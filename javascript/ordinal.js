dd94bf0673575797e4726e32f2c7465ce7c1bb86
gamer0kayf1n3 <81130943+gamer0kayf1n3@users.noreply.github.com>
2021-06-30 14:54:54 +0800

    Javascript

---
+++ javascript/ordinal.js
@@ -0,0 +1,33 @@
+function ordinal() {
+	var number = prompt("Ordinal of?");
+	var number_str = String(number);
+	var digits = number_str.split('');
+	var suffix;
+	if (digits.length == 1) {
+		if (digits[digits.length - 1] == "1") {
+			suffix = "st";
+		} else if (digits[digits.length - 1] == "2") {
+			suffix = "nd";
+		} else if (digits[digits.length - 1] == "3") {
+			suffix = "rd";
+		} else {
+			suffix = "th";
+		}
+
+	} else if (digits[digits.length - 2] == "1") {
+		suffix = "th";
+	} else {
+		if (digits[digits.length - 1] == "1") {
+			suffix = "st";
+		} else if (digits[digits.length - 1] == "2") {
+			suffix = "nd";
+		} else if (digits[digits.length - 1] == "3") {
+			suffix = "rd";
+		} else {
+			suffix = "th";
+		}
+
+	}
+
+	console.log(number_str + suffix);
+}
