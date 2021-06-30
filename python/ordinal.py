62003b1b2c9d703b42bc3eb1928db2fef9a9b7cc
gamer0kayf1n3 <81130943+gamer0kayf1n3@users.noreply.github.com>
2021-06-30 13:19:02 +0800

    Create ordinal.py

---
+++ python/ordinal.py
@@ -0,0 +1,33 @@
+while True:
+    try:
+        number= int(input('Ordinal of: '))
+        number_str = str(number)
+        st, nd, rd, th = 'st', 'nd', 'rd', 'th'
+        digits = []
+        for x in number_str:
+            digits.append(int(x))
+        suffix = None
+        if len(digits) == 1:
+            if digits[-1] == 1:
+                suffix = st
+            elif digits[-1] == 2:
+                suffix = nd
+            elif digits[-1] == 3:
+                suffix = rd
+            else:
+                suffix = th
+        elif digits[-2] == 1:
+            suffix = th
+        else:
+            if digits[-1] == 1:
+                suffix = st
+            elif digits[-1] == 2:
+                suffix = nd
+            elif digits[-1] == 3:
+                suffix = rd
+            else:
+                suffix = th
+        print(f'{number}{suffix}')
+    except (ValueError,TypeError):
+        print('Invalid')
+        pass
